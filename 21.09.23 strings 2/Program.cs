3d0cd4093f6fb928fb7a8d1f285b82293e86e818
Liam Bolan <AQ231688@aquinas.ac.uk>
2023-09-21 13:43:07 +0100

    Add project files.

---
+++ 21.09.23 strings 2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21._09._23_strings_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { string movie1 = "Oppenheimer (R)";
+          string movie2 = "Thor: Love and Thunder(12)";
+            string movie3 = "Strays (15)";
+          string movie4 = 
+          string movie5 =
+        } 
+        int age = int.Parse(Console.ReadLine());
+
+    }
+}
